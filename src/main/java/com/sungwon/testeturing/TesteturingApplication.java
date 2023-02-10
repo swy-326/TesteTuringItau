d64039e7aa1d39f879c10dc0d87ae19e2ffbb9fd
swy-326 <sungwon.yoon@usp.br>
2023-02-10 20:59:03 -0300

    initial commit

---
+++ src/main/java/com/sungwon/testeturing/TesteturingApplication.java
@@ -0,0 +1,13 @@
+package com.sungwon.testeturing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteturingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteturingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungwon/testeturing/TesteturingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungwon.testeturing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteturingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
